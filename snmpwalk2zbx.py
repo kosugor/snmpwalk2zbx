c5a091280773f01cdca3b3fa8a18115e01970e91
Goran Kosutic <kosuticg@yahoo.com>
2022-12-03 18:14:28 +0100

    formatting

--- snmpwalk2zbx.py
+++ snmpwalk2zbx.py
@@ -1,19 +1,24 @@
 # snmpwalk2zbx: Create a Zabbix template from an SNMPWALK response.
 # Copyright (C) Goran Kosutic 2022
-#Sorce https://github.com/kosugor/snmpwalk2zbx
+# Sorce https://github.com/kosugor/snmpwalk2zbx
 
-import os,sys,re
+
+import os, sys, re
+
 
 def WalkResponse(community, snmpver, port, IP, oid):
-    if snmpver=="1" or snmpver=="2c":
-        walkresponse = os.popen('snmpwalk -v '+ snmpver +' -c ' + community + ' -On ' + IP + ' ' + oid).read()
+    if snmpver == "1" or snmpver == "2c":
+        walkresponse = os.popen(
+            "snmpwalk -v " + snmpver + " -c " + community + " -On " + IP + " " + oid
+        ).read()
         return walkresponse
 
-snmpver="2c"
-community="public"
-port="161"
-IP="127.0.0.1"
-oid=".1.3.6.1"
 
-r=WalkResponse(community,snmpver,port,IP,oid)
+snmpver = "2c"
+community = "public"
+port = "161"
+IP = "127.0.0.1"
+oid = ".1.3.6.1"
+
+r = WalkResponse(community, snmpver, port, IP, oid)
 print(r)
