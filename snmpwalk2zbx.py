3560414a82c258d040df93140c4505fbc3f443a7
Goran Kosutic <kosuticg@yahoo.com>
2022-12-03 17:27:20 +0100

    first test

--- snmpwalk2zbx.py
+++ snmpwalk2zbx.py
@@ -4,8 +4,16 @@
 
 import os,sys,re
 
-def WalkResponse(community, snmpver, port, ip, oid):
-    if snmpver=="1" or snmpver=="2c"):
+def WalkResponse(community, snmpver, port, IP, oid):
+    if snmpver=="1" or snmpver=="2c":
         walkresponse = os.popen('snmpwalk -v '+ snmpver +' -c ' + community + ' -On ' + IP + ' ' + oid).read()
         return walkresponse
 
+snmpver="2c"
+community="public"
+port="161"
+IP="127.0.0.1"
+oid=".1.3.6.1"
+
+r=WalkResponse(community,snmpver,port,IP,oid)
+print(r)
