ef0f1474e73cbf7290679c97a2785eff80c782ec
Goran Kosutic <kosuticg@yahoo.com>
2022-12-08 00:21:47 +0100

    setup class, etc

--- walk4zbx.py
+++ walk4zbx.py
@@ -1,8 +1,6 @@
 # snmpwalk2zbx: Create a Zabbix template from an SNMPWALK response.
 # Copyright (C) Goran Kosutic 2022
 # Sorce https://github.com/kosugor/walk4zbx
-
-
 import sys
 import getopt
 import re
@@ -10,45 +8,54 @@ from os import popen
 from html import escape
 from collections import OrderedDict
 
-# default values
-VER = "2c"
-COMMUNITY = "public"
-AGENT = "127.0.0.1"
-PORT = "161"
-OID = ["."]
 
-ver = VER
-community = COMMUNITY
-agent = AGENT
-port = PORT
+DESCpattern = re.compile(r'DESCRIPTION\s+"([^"]*)"')
+OIDpattern = re.compile(r"^(\.[0-9]+)+")
+MSpattern = re.compile(r"^(\S*)::(\S*)")
 
-detailpattern = re.compile(r'DESCRIPTION\s+"([^"]*)"')
-oidpattern = re.compile(r"(\.[0-9]+)+")
 
-try:
-    myopts, args = getopt.getopt(sys.argv[1:], "v:c:a:p")
-except getopt.GetoptError as e:
-    print(str(e))
-    print(f"Usage: {sys.argv[0]} -v 1|2c|3 -c COMMUNITY -a AGENT -p PORT OID [OID2...]")
-    sys.exit(2)
+class Setup:
+    def __init__(self):
+        # default values
+        VER = "2c"
+        COMMUNITY = "public"
+        AGENT = "127.0.0.1"
+        PORT = "161"
+        BASEOIDS = [
+            ".",
+        ]
+        self.ver = VER
+        self.community = COMMUNITY
+        self.agent = AGENT
+        self.port = PORT
+        self.baseoids = BASEOIDS
 
-for option, argument in myopts:
-    match option:
-        case "-v":
-            if argument in ["1", "2c", "3"]:
-                ver = argument
-            else:
-                print("snmp version not recognized, using default (2c)")
-        case "-c":
-            community = argument
-        case "-a":
-            agent = argument
-        case "-p":
-            port = argument
+    def readParams(self):
+        try:
+            myopts, args = getopt.getopt(sys.argv[1:], "v:c:a:p")
+        except getopt.GetoptError as e:
+            print(str(e))
+            print(
+                f"Usage: {sys.argv[0]} -v 1|2c|3 -c COMMUNITY -a AGENT -p PORT OID [OID2...]"
+            )
+            sys.exit(2)
 
-baseoids = args
-if len(baseoids) == 0:
-    baseoids = OID
+        for option, argument in myopts:
+            match option:
+                case "-v":
+                    if argument in ["1", "2c", "3"]:
+                        self.ver = argument
+                    else:
+                        print("snmp version not recognized, using default (2c)")
+                case "-c":
+                    self.community = argument
+                case "-a":
+                    self.agent = argument
+                case "-p":
+                    self.port = argument
+
+        if len(args) != 0:
+            self.baseoids = args
 
 
 def WalkResponse(v, c, a, p, o):
@@ -56,15 +63,35 @@ def WalkResponse(v, c, a, p, o):
         command = f"snmpwalk -v {v} -c {c} -On {a}:{p} {o}"
         print("USING: " + command)
         wr = popen(command).read()
-        return wr
+        return wr.rstrip()
 
 
 def OIDtranslate(o):
     # returns a tuple (fullname, details)
     transfull = popen(f"snmptranslate -Of {o}").read()
     transdetail = popen(f"snmptranslate -Td -OS {o}").read()
-    print(transdetail)
-    return transfull.rstrip(), transdetail.rstrip()
+    # determine MIB and short symbolic name
+    ms = findMS(transdetail)
+    return transfull.rstrip(), transdetail.rstrip(), ms[0], ms[1]
+
+
+def findLevels(o):
+    l = o.split(".")
+    return len(l)
+
+
+def uplevels(o, n):
+    newoid = o
+    for l in range(1, n + 1):
+        n = newoid.rfind(".")
+        newoid = newoid[:n]
+    return newoid
+
+
+def lastLevel(o):
+    t = o.rfind(".") + 1
+    ll = o[t:]
+    return ll
 
 
 def BelongsToTable(k):
@@ -86,42 +113,54 @@ def FindColumnName(k):
     return ".".join(co)
 
 
-def Details2html(det):
-    detsearch = detailpattern.search(det)
+def desc2html(det):
+    detsearch = DESCpattern.search(det)
     dettext = detsearch.group(1)
     return escape(dettext)
+
+
+def findMS(det):
+    MSmatch = MSpattern.match(det)
+    if MSmatch:
+        return MSmatch.groups()
 
 
 checkedoids = OrderedDict()
 scalars = []
 columnoids = []
 
-for baseoid in baseoids:
-    response = WalkResponse(ver, community, agent, port, baseoid).rstrip()
-    responselines = response.split("\n")
+s = Setup()
+s.readParams()
+
+for baseoid in s.baseoids:
+    response = WalkResponse(s.ver, s.community, s.agent, s.port, baseoid)
+    responselines = response.splitlines()
     print(f"Response has {len(responselines)} lines")
     for responseline in responselines:
         # checking if it is a numeric oid in the beginning of response line
-        if oidpattern.match(responseline):
-            # taking only numeric oid
-            currentoid = responseline.split("=")[0].rstrip()
+        oidmatch = OIDpattern.match(responseline)
+        if oidmatch:
+            currentoid = oidmatch.group()
             # avoiding duplicate oids
             if currentoid not in checkedoids:
-                trfull, trdetail = OIDtranslate(currentoid)
+                trfull, trdetail, m, s = OIDtranslate(currentoid)
                 checkedoids[currentoid] = trfull, trdetail
                 if not BelongsToTable(currentoid):
-                    scalars.append(currentoid)
-                    # this is not correct parent, first it needs to check how many oid levels in MIB::oid
-                    par = currentoid[: currentoid.rfind(".")]
-                    print(f"ADDING SIMPLE ITEM {currentoid}, PARENT {par}")
+                    # first it needs to check how many oid levels in short oid
+                    levelsShort = findLevels(s)
+                    parfull = uplevels(trfull, levelsShort)
+                    parshort = lastLevel(parfull)
+                    desc = desc2html(trdetail)
+                    scalars.append((parshort, s, currentoid, trfull, desc))
+                    print(
+                        f"ADDING SIMPLE ITEM: APP {parshort} ITEM {m}::{s} KEY {currentoid}\n{trfull}\n{desc}\n"
+                    )
                 else:
                     CN = FindColumnName(currentoid)
                     if CN not in columnoids:
                         columnoids.append(CN)
-                        E = CN[: CN.rfind(".")]
-                        ti = E.rfind(".")
-                        TN = CN[:ti]
-                        print(f"TABLE {TN} COLUMN {CN}")
+                        TN = uplevels(CN, 2)
+                        # print(f"TABLE {TN} COLUMN {CN}")
         else:
             print("discarding line: '" + responseline + "'")
     print("")
@@ -130,8 +169,3 @@ print(f"Total OIDs: {len(checkedoids)}")
 
 print("Simple items:", len(scalars))
 print("Columns:", len(columnoids))
-
-"""
-for sc in scalars:
-    print(checkedoids[sc][0])
-"""
